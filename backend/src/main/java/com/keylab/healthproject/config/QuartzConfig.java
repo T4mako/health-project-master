dcd5290f022cf3e7c8484ad7fda15041e7c8de16
T4mako <1536954632@qq.com>
2025-01-07 10:23:15 +0800

    添加数字孪生场景，更改定时任务

---
+++ backend/src/main/java/com/keylab/healthproject/config/QuartzConfig.java
@@ -0,0 +1,8 @@
+package com.keylab.healthproject.config;
+
+/**
+ * @author T4mako
+ * @date 2025/1/5 17:59
+ */
+public class QuartzConfig {
+}
---
+++ backend/src/main/java/com/keylab/healthproject/task/FetchDataJob.java
@@ -0,0 +1,8 @@
+package com.keylab.healthproject.task;
+
+/**
+ * @author T4mako
+ * @date 2025/1/5 18:00
+ */
+public class FetchDataJob {
+}
